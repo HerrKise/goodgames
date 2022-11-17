954136e9e2cc36d6825ec84e98770551eb8fc07b
HerrKise <oskarwilde777@gmail.com>
2022-11-17 21:13:01 +0300

    hotfix authservice

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,11 +1,18 @@
+import { useDispatch } from "react-redux";
+import { register } from "../store/reducers/userSlice";
+
 export default function () {
-    return(
+    return (
         <section className="w-[100%] bg-black">
             <div className="w-[1024px] mx-auto h-[900px] flex flex-col justify-center items-center">
                 <form className="bg-[#161823] w-[500px] h-[600px] py-[30px] px-[20px] rounded-[20px]">
                     <div className="flex flex-col justify-center items-center border-b-[1px] border-gray-400 pb-[10px]">
-                        <h2  className="text-white">Добро пожаловать на Good Games!</h2>
-                        <p className="text-gray-500">Пожалуйста указывайте только достоверные данные!</p>
+                        <h2 className="text-white">
+                            Добро пожаловать на Good Games!
+                        </h2>
+                        <p className="text-gray-500">
+                            Пожалуйста указывайте только достоверные данные!
+                        </p>
                     </div>
                     <div className="flex flex-col items-center justify-between h-[70%] py-[20px] border-b-[1px] border-gray-400">
                         <div className="flex justify-between w-[80%] items-center">
@@ -42,7 +49,9 @@ export default function () {
                             />
                         </div>
                         <div className="flex justify-between w-[80%] items-center">
-                            <label className="text-gray-500">Повторите пароль</label>
+                            <label className="text-gray-500">
+                                Повторите пароль
+                            </label>
                             <input
                                 type="password-rep"
                                 id="password-rep"
@@ -64,20 +73,21 @@ export default function () {
                                 className="border-[1px] w-[200px] border-[#9e9e9e] rounded-[5px] text-[16px] h-[40px] px-[20px] bg-gray-600"
                             />
                         </div>
-
-
                     </div>
                     <div className="flex justify-between items-center">
                         <div className="flex w-[250px] items-center justify-between">
-                            <input type="checkbox" required/>
-                            <label className="text-gray-500 ml-[20px]">Я подтверждаю, что согласен
-                                с пользовательским соглашением и
-                                политикой конфиденциальности</label>
+                            <input type="checkbox" required />
+                            <label className="text-gray-500 ml-[20px]">
+                                Я подтверждаю, что согласен с пользовательским
+                                соглашением и политикой конфиденциальности
+                            </label>
                         </div>
-                        <button className="bg-amber-500 w-[150px] h-[50px] rounded-[10px]">Готово!</button>
+                        <button className="bg-amber-500 w-[150px] h-[50px] rounded-[10px]">
+                            Готово!
+                        </button>
                     </div>
                 </form>
             </div>
         </section>
-    )
+    );
 }
