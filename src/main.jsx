d7620f727e1112fc0764ff8dd0ec7f9f4aec81fa
a0ne <mako666777@gmail.com>
2022-11-17 19:42:25 +0300

    fx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import { setupStore } from "./store";
+import { BrowserRouter } from "react-router-dom";
 
 const store = setupStore();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
+        <BrowserRouter>
         <Provider store={store}>
             <App />
         </Provider>
+        </BrowserRouter>
     </React.StrictMode>
 );
