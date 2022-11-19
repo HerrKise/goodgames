ae39f80a1bfb6922fc35808c5ce873b0e8566586
HerrKise <oskarwilde777@gmail.com>
2022-11-19 16:26:21 +0300

    login and regiter methods are successfully connected with backend

--- src/services/localStorage.service.js
+++ src/services/localStorage.service.js
@@ -1,9 +1,7 @@
 const TOKEN_KEY = "jwt-token";
 const REFRESH_KEY = "jwt-refresh-token";
-const USERID_KEY = "user-local-id";
 
-export function setTokens({ refreshToken, accessToken, userId }) {
-    localStorage.setItem(USERID_KEY, userId);
+export function setTokens(refreshToken, accessToken) {
     localStorage.setItem(TOKEN_KEY, accessToken);
     localStorage.setItem(REFRESH_KEY, refreshToken);
 }
@@ -14,18 +12,16 @@ export function getRefreshToken() {
     return localStorage.getItem(REFRESH_KEY);
 }
 export function removeAuthData() {
-    localStorage.removeItem(USERID_KEY);
     localStorage.removeItem(TOKEN_KEY);
     localStorage.removeItem(REFRESH_KEY);
 }
-export function getUserId() {
+/* export function getUserId() {
     return localStorage.getItem(USERID_KEY);
-}
+} */
 const localStorageService = {
     setTokens,
     getAccessToken,
     getRefreshToken,
-    getUserId,
     removeAuthData
 };
 export default localStorageService;
--- src/store/reducers/userSlice.js
+++ src/store/reducers/userSlice.js
@@ -7,12 +7,14 @@ const initialState = localStorageService.getAccessToken()
           entities: null,
           isLoading: true,
           error: null,
+          isLoggedIn: true,
           auth: { userId: localStorageService.getUserId() }
       }
     : {
           entities: null,
           isLoading: false,
           error: null,
+          isLoggedIn: false,
           auth: null
       };
 
@@ -20,15 +22,15 @@ export const userSlice = createSlice({
     name: "user",
     initialState,
     reducers: {
-        login: async (state, action) => {
-            try {
-                const responce = await authService.login(action.payload);
-                localStorage.setItem(responce.data.tokens); // пусть пока токены будут в таком объекте
-                state.entities = responce.data.user;
-                state.isLoading = false;
-            } catch (e) {
-                console.log(e.responce?.data?.message);
-            }
+        authRequested: (state) => {
+            state.error = null;
+        },
+        authRequestSuccess: (state, action) => {
+            //тут возможно понадобиться еще через пейлоад передавать юзерайди, который надо будет отдельно полкчить по пост запросу
+            state.isLoggedIn = true;
+        },
+        authRequestFailed: (state, action) => {
+            state.error = action.payload;
         },
         logout: (state, action) => {
             state.entities = null;
@@ -40,16 +42,41 @@ export const userSlice = createSlice({
 });
 
 export const { reducer: userReducer, actions } = userSlice;
-export const { login, logout } = actions;
+export const { logout, authRequested, authRequestSuccess, authRequestFailed } =
+    actions;
+
+export const signIn = (payload) => async (dispatch) => {
+    dispatch(authRequested());
+    try {
+        const response = await authService.login(payload);
+        localStorageService.setTokens(
+            response.refreshToken,
+            response.accessToken
+        ); // пусть пока токены будут в таком объекте
+        /* state.entities = responce.data.user;
+        state.isLoading = false; */
+        dispatch(authRequestSuccess());
+    } catch (e) {
+        console.log(e);
+        dispatch(authRequestFailed(e.response.data.errors));
+    }
+};
 
 export const register = (payload) => async (dispatch) => {
+    dispatch(authRequested());
     try {
-        const responce = await authService.register(payload);
-        console.log(responce);
-        localStorage.setItem(responce.data?.tokens); // пусть пока токены будут в таком объекте
+        const response = await authService.register(payload);
+        console.log(response);
+        localStorageService.setTokens(
+            response.refreshToken,
+            response.accessToken
+        );
+        dispatch(authRequestSuccess());
+        // пусть пока токены будут в таком объекте
         /* state.entities = responce.data.user;
         state.isLoading = false; */
     } catch (e) {
-        console.log(e.responce?.data?.message);
+        console.log(e);
+        dispatch(authRequestFailed(e.response.data.errors));
     }
 };
