7c6f6a29b9c4109b056e18881feb538db9214eb6
a0ne <mako666777@gmail.com>
2022-11-19 21:10:06 +0300

    chpassword

---
+++ src/pages/PasswordChange.jsx
@@ -0,0 +1,43 @@
+import {useState} from "react";
+
+export const PasswordChange = () => {
+
+    const dispatch = useDispatch();
+    const [password, setpassword] = useState("");
+    const [newpassword, setnewpassword] = useState("");
+    const [confirmnewpassword, setconfirmnewpassword] = useState("");
+
+    const changepassword = (e) => {
+        setpassword(e.target.value);
+    };
+    const changenewpassword = (e) => {
+        setnewpassword(e.target.value);
+    };
+    const changeconfirmnewpassword = (e) => {
+        setconfirmnewpassword(e.target.value);
+    };
+
+    const handleChangePassword = () => {
+        dispatch(
+            updatePassword({
+                currentPassword: password,
+                newPassword: newpassword,
+                confirmNewPassword: confirmnewpassword
+            })
+        );
+    };
+
+    return(
+        <div>
+            <input value={password} onChange={changepassword} />
+            <input value={newpassword} onChange={changenewpassword} />
+            <input
+                value={confirmnewpassword}
+                onChange={changeconfirmnewpassword}
+            />
+            <button onClick={handleChangePassword}>Поменять пароль</button>
+        </div>
+    )
+}
+
+
