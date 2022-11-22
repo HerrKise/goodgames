1498a9083b83ab31ac855b8d0edf86093bd17827
HerrKise <oskarwilde777@gmail.com>
2022-11-22 16:02:06 +0300

    added services for email confirmation

--- src/services/staff.service.js
+++ src/services/staff.service.js
@@ -1,32 +1,43 @@
 import http from "../http";
 import localStorageService from "./localStorage.service";
 
-const staffEndpoint = "staff/";
+const staffEndpoint = "/Authenticate";
 
-const staffService = {
-    login: async (email, password) => {
-        const { data } = await http.post(staffEndpoint + "login", {
-            email,
-            password
-        });
+const authService = {
+    login: async (payload) => {
+        const { data } = await http.post(staffEndpoint + "/Login", payload);
         return data;
     },
-    register: async (email, role, password, login) => {
-        const { data } = await http.post(staffEndpoint + "register", {
-            email,
-            role,
-            password,
-            login
-        });
+    register: async (payload) => {
+        const { data } = await http.post(staffEndpoint + "/Register", payload);
+        console.log(data);
         return data;
     },
     refresh: async () => {
-        const { data } = await http.post("token", {
-            grant_type: "refresh_token",
-            refresh_token: localStorageService.getRefreshToken()
+        const { data } = await http.post(staffEndpoint + "/RefreshToken", {
+            token: localStorageService.getAccessToken(),
+            refreshToken: localStorageService.getRefreshToken()
         });
+        return data;
+    },
+    getUserId: async () => {
+        const { data } = await http.get(staffEndpoint + "/GetMe");
+        return data;
+    },
+    generateRestorePassword: async (payload) => {
+        const { data } = await http.post(
+            staffEndpoint + "/GenerateRestorePassword",
+            payload
+        );
+        return data;
+    },
+    restorePassword: async (payload) => {
+        const { data } = await http.post(
+            staffEndpoint + "/RestorePassword",
+            payload
+        );
         return data;
     }
 };
 
-export default staffService;
+export default authService;
--- src/store/reducers/staffSlice.js
+++ src/store/reducers/staffSlice.js
@@ -1,18 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
-import staffService from "../../services/staff.service";
+import { createSlice, createAction } from "@reduxjs/toolkit";
+import authService from "../../services/auth.service";
 import localStorageService from "../../services/localStorage.service";
+import userService from "../../services/user.service";
 
 const initialState = localStorageService.getAccessToken()
     ? {
           entities: null,
           isLoading: true,
           error: null,
+          isLoggedIn: true,
           auth: { userId: localStorageService.getUserId() }
       }
     : {
           entities: null,
           isLoading: false,
           error: null,
+          isLoggedIn: false,
           auth: null
       };
 
@@ -20,41 +23,158 @@ export const staffSlice = createSlice({
     name: "staff",
     initialState,
     reducers: {
-        login: async (state, action) => {
-            const { email, password } = action.payload;
-            try {
-                const responce = await staffService.login(email, password);
-                localStorage.setItem(responce.data.tokens); // пусть пока токены будут в таком объекте
-                state.entities = responce.data.staff; // мб responce.data.user, уточнить с бэком
-                state.isLoading = false;
-            } catch (error) {
-                console.log(e.responce?.data?.message);
-            }
+        staffRequested: (state) => {
+            state.isLoading = true;
         },
-        register: async (state, action) => {
-            const { email, role, password, login } = action.payload;
-            try {
-                const responce = await authService.register(
-                    email,
-                    role,
-                    password,
-                    login
-                );
-                localStorage.setItem(responce.data.tokens); // пусть пока токены будут в таком объекте
-                state.entities = responce.data.staff; // мб responce.data.user, уточнить с бэком
-                state.isLoading = false;
-            } catch (error) {
-                console.log(e.responce?.data?.message);
-            }
+        staffReceived: (state, action) => {
+            state.entities = action.payload;
+            state.isLoading = false;
+        },
+        staffRequestFailed: (state, action) => {
+            state.error = action.payload;
+            state.isLoading = false;
+        },
+        authRequested: (state) => {
+            state.error = null;
+        },
+        authRequestSuccess: (state, action) => {
+            //тут возможно понадобиться еще через пейлоад передавать юзерайди, который надо будет отдельно полкчить по пост запросу
+            state.isLoggedIn = true;
+            state.isLoading = false;
+        },
+        authRequestFailed: (state, action) => {
+            state.error = action.payload;
+            state.isLoading = false;
         },
         logout: (state, action) => {
             state.entities = null;
             state.auth = null;
             state.isLoading = false;
+            state.isLoggedIn = false;
             localStorageService.removeAuthData();
         }
     }
 });
 
-export const { reducer: staffReducer, actions } = staffSlice;
-export const { login, register, logout } = actions;
+export const { reducer: userReducer, actions } = userSlice;
+export const {
+    logout,
+    authRequested,
+    authRequestSuccess,
+    authRequestFailed,
+    staffRequested,
+    staffReceived,
+    staffRequestFailed
+} = actions;
+
+const editUserProfileRequested = createAction("user/editUserProfileRequested");
+const editUserProfileFailed = createAction("user/editUserProfileFailed");
+const editUserProfileSuccess = createAction("user/editUserProfileSuccess");
+
+const updatePasswordRequested = createAction("user/updatePasswordRequested");
+const updatePasswordFailed = createAction("user/updatePasswordFailed");
+const updatePasswordSuccess = createAction("user/updatePasswordSuccess");
+
+const updatePictureRequested = createAction("user/updatePictureRequested");
+const updatePictureFailed = createAction("user/updatePictureFailed");
+const updatePictureSuccess = createAction("user/updatePictureSuccess");
+
+export const signIn = (payload) => async (dispatch) => {
+    dispatch(authRequested());
+    try {
+        const { refreshToken, accessToken } = await authService.login(payload);
+        localStorageService.setTokens(refreshToken, accessToken);
+        const { userId } = await authService.getUserId();
+        localStorageService.setUserId(userId);
+        dispatch(authRequestSuccess());
+    } catch (e) {
+        console.log(e);
+        dispatch(authRequestFailed(e.response.data.errors));
+    }
+};
+
+export const register = (payload) => async (dispatch) => {
+    dispatch(authRequested());
+    try {
+        const { refreshToken, accessToken } = await authService.login(payload);
+        localStorageService.setTokens(refreshToken, accessToken);
+
+        const { userId } = await authService.getUserId();
+        localStorageService.setUserId(userId);
+        dispatch(authRequestSuccess());
+    } catch (e) {
+        console.log(e);
+        dispatch(authRequestFailed(e.response.data.errors));
+    }
+};
+
+export const loadUserProfile = (payload) => async (dispatch) => {
+    dispatch(userRequested());
+    try {
+        const data = await userService.getProfile(payload);
+        dispatch(userReceived(data));
+        console.log(data);
+    } catch (e) {
+        console.log(e);
+        dispatch(usersRequestFailed(e.response.data.errors));
+    }
+};
+
+export const editUserProfile = (payload) => async (dispatch) => {
+    dispatch(editUserProfileRequested());
+    try {
+        const data = await userService.editProfile(payload);
+        dispatch(editUserProfileSuccess());
+    } catch (e) {
+        console.log(e);
+        dispatch(editUserProfileFailed());
+    }
+};
+
+export const updatePassword = (payload) => async (dispatch) => {
+    dispatch(updatePasswordRequested());
+    try {
+        const data = await userService.updatePassword(payload);
+        dispatch(updatePasswordSuccess());
+    } catch (e) {
+        console.log(e);
+        dispatch(updatePasswordFailed());
+    }
+};
+
+export const generateRestorePassword = (payload) => async (dispatch) => {
+    dispatch(updatePasswordRequested());
+    try {
+        const data = await authService.generateRestorePassword(payload);
+        dispatch(updatePasswordSuccess());
+    } catch (e) {
+        console.log(e);
+        dispatch(updatePasswordFailed());
+    }
+};
+
+export const restorePassword = (payload) => async (dispatch) => {
+    dispatch(updatePasswordRequested());
+    try {
+        const data = await authService.restorePassword(payload);
+        dispatch(updatePasswordSuccess());
+    } catch (e) {
+        console.log(e);
+        dispatch(updatePasswordFailed());
+    }
+};
+
+export const updatePicture = (payload) => async (dispatch) => {
+    dispatch(updatePictureRequested());
+    try {
+        const data = await userService.updateProfilePicture(payload);
+        dispatch(updatePictureSuccess());
+    } catch (e) {
+        console.log(e);
+        dispatch(updatePictureFailed());
+    }
+};
+
+export const getUserProfileData = () => (state) => state.user.entities;
+export const getUserLoadingStatus = () => (state) => state.user.isLoading;
+export const getIsLoggedIn = () => (state) => state.user.isLoggedIn;
