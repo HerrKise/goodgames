4e3dab639b07dcfa6ebfedd081dcb1aafa4613ce
HerrKise <oskarwilde777@gmail.com>
2022-11-23 16:13:24 +0300

    added functional to refresh tokens depending on the role

--- src/services/staff.service.js
+++ src/services/staff.service.js
@@ -1,18 +1,18 @@
 import http from "../http";
 import localStorageService from "./localStorage.service";
 
-const staffEndpoint = "/Authenticate";
+const staffEndpoint = "/AdminPanel";
 
-const authService = {
+const staffService = {
     login: async (payload) => {
         const { data } = await http.post(staffEndpoint + "/Login", payload);
         return data;
     },
-    register: async (payload) => {
+    /* register: async (payload) => {
         const { data } = await http.post(staffEndpoint + "/Register", payload);
         console.log(data);
         return data;
-    },
+    }, */
     refresh: async () => {
         const { data } = await http.post(staffEndpoint + "/RefreshToken", {
             token: localStorageService.getAccessToken(),
@@ -20,24 +20,10 @@ const authService = {
         });
         return data;
     },
-    getUserId: async () => {
+    getProfile: async () => {
         const { data } = await http.get(staffEndpoint + "/GetMe");
-        return data;
-    },
-    generateRestorePassword: async (payload) => {
-        const { data } = await http.post(
-            staffEndpoint + "/GenerateRestorePassword",
-            payload
-        );
-        return data;
-    },
-    restorePassword: async (payload) => {
-        const { data } = await http.post(
-            staffEndpoint + "/RestorePassword",
-            payload
-        );
         return data;
     }
 };
 
-export default authService;
+export default staffService;
