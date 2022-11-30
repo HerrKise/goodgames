259deac7626ccbecd492d825be7f227080abc54f
Максим <maksim@Maksims-MacBook-Pro.local>
2022-12-01 02:17:28 +0300

    +

--- src/pages/TournamentCreation.jsx
+++ src/pages/TournamentCreation.jsx
@@ -1,20 +1,57 @@
+import { useState } from "react";
+
 const TournamentCreation = () => {
+    const [mods, setMods] = useState([]);
     return (
         <section className="bg-grey-300 w-[100%] min-h-[100vh]">
             <div className="w-[1024px] mx-auto flex flex-col items-center">
                 <h2>Создание ивента</h2>
                 <form>
-                    <div>
+                    <div className="flex flex-col items-center">
                         <p>Тип ивента</p>
                         <select>
                             <option value="tour">Турнир</option>
-                            <option value="tour">Мини-турнир</option>
-                            <option value="tour">Практис</option>
+                            <option value="mtour">Мини-турнир</option>
+                            <option value="practice">Практис</option>
                         </select>
                         <p>Дата</p>
                         <input type="date"></input>
                         <p>Время</p>
                         <input type="time"></input>
+                        <p>Модератор</p>
+                        <select>
+                            <option>mod1</option>
+                            <option>mod2</option>
+                            <option>mod3</option>
+                            <option>mod4</option>
+                            <option>mod5</option>
+                        </select>
+                        <p>Тип матча</p>
+                        <select>
+                            <option>Free</option>
+                            <option>Paid</option>
+                        </select>
+                        <p>Карта</p>
+                        <select>
+                            <option>Erangel</option>
+                            <option>Мирамар</option>
+                            <option>Санук</option>
+                            <option>Викенди</option>
+                            <option>Ливик</option>
+                            <option>Arcade Эрангель</option>
+                            <option>TDM Склад</option>
+                        </select>
+                        <p>Режим</p>
+                        <select>
+                            <option>Solo</option>
+                            <option>Duo</option>
+                            <option>Squad</option>
+                        </select>
+                        <p>Вид</p>
+                        <select>
+                            <option>1pw</option>
+                            <option>3pw</option>
+                        </select>
                     </div>
                 </form>
             </div>
