5f2974b1eb4c8a300715784b88f0da80124b5130
HerrKise <oskarwilde777@gmail.com>
2022-12-16 15:43:41 +0300

    fix to build

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import { setupStore } from "./store";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import "../index.css";
 
 const store = setupStore();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App />
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 );
